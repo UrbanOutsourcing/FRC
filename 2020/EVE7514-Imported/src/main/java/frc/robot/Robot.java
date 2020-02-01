63286837d1edac9b83b7080b0749003bc48b4f0f
nubian1252 <michael@prude.org>
2020-02-01 16:59:34 -0500

    Initial Source Base

---
+++ 2020/EVE7514-Imported/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,120 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.CameraServer;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Lift;
+import frc.robot.subsystems.Pivot;
+import frc.robot.subsystems.HatchArm;
+import frc.robot.subsystems.*;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends TimedRobot {
+  Command m_autonomousCommand;
+
+  public static DriveTrain m_drivetrain;
+  public static eDriveTrain m_edrivetrain;
+  public static mDriveTrain m_mdrivetrain;
+  public static Pivot m_pivot;
+  public static Lift m_lift;
+  public static Shooter m_shooter;
+  public static HatchArm m_hatcharm;
+  public static OI m_oi;
+
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+  @Override
+  public void robotInit() {//
+    // Initialize all subsystems
+    m_drivetrain = new DriveTrain();
+    //m_edrivetrain = new eDriveTrain();
+     //m_edrivetrain = new mDriveTrain();
+    m_pivot = new Pivot();
+    m_lift = new Lift();
+    m_shooter = new Shooter();
+    m_hatcharm = new HatchArm();
+    m_oi = new OI();
+    CameraServer.getInstance().startAutomaticCapture();
+
+    // instantiate the command used for the autonomous period
+
+    // Show what command your subsystem is running on the SmartDashboard
+    //SmartDashboard.putData(m_edrivetrain);
+    SmartDashboard.putData(m_drivetrain);
+    SmartDashboard.putData(m_lift);
+    SmartDashboard.putData(m_pivot);
+    SmartDashboard.putData(m_hatcharm);
+    SmartDashboard.putData(m_shooter);
+  }
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand.start(); // schedule the autonomous command (example)
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    Scheduler.getInstance().run();
+    log();
+  }
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    // m_autonomousCommand.cancel();
+  }
+
+  /**
+   * This function is called periodically during teleoperated mode.
+   */
+  @Override
+  public void teleopPeriodic() {
+    Scheduler.getInstance().run();
+    log();
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
+
+  /**
+   * The log method puts interesting information to the SmartDashboard.
+   */
+  private void log() {
+    m_lift.log();
+    m_pivot.log();
+    m_drivetrain.log();
+    //m_edrivetrain.log();
+    //m_mdrivetrain.log();
+    m_shooter.log();
+    m_hatcharm.log();
+
+  }
+}
---
+++ 2020/EVE7514-Imported/src/main/java/frc/robot/subsystems/Pivot.java
@@ -0,0 +1,127 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.robot.Constants;
+import frc.robot.commands.*;
+
+/**
+ * The lift subsystem uses PID to go to a given height. Unfortunately, in it's
+ * current state PID values for simulation are different than in the real world
+ * do to minor differences.
+ */
+public class Pivot extends Subsystem {
+  private final TalonSRX m_motor;
+  // public final DigitalInput m_toplimitswitch,m_bottomlimitswitch ;
+
+  /**
+   * Create a new pivot subsystem.
+   */
+  public Pivot() {
+    super();
+
+    m_motor = new TalonSRX(RobotMap.PIVOT);
+
+    // m_toplimitswitch = new DigitalInput(RobotMap.TOP_LIMITSWITCH);
+    // m_bottomlimitswitch = new DigitalInput(RobotMap.BOTTOM_LIMITSWITCH);
+
+    // *TalonSRX Encoder Configuration
+    // *Config the sensor used for Primary PID and sensor direction
+
+    m_motor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Constants.kPIDPivot,
+        Constants.kTimeoutMs);
+
+    // Ensure sensor is positive when output is positive
+
+    m_motor.setSensorPhase(Constants.kSensorPhase);
+    /**
+     * Set based on what direction you want forward/positive to be. This does not
+     * affect sensor phase.
+     */
+    m_motor.setInverted(Constants.kMotorInvert);
+
+    // * Config the peak and nominal outputs, 12V means full
+    m_motor.configNominalOutputForward(0, Constants.kTimeoutMs);
+    m_motor.configNominalOutputReverse(0, Constants.kTimeoutMs);
+    m_motor.configPeakOutputForward(1, Constants.kTimeoutMs);
+    m_motor.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+
+    /**
+     * Config the allowable closed-loop error, Closed-Loop output will be neutral
+     * within this range. See Table in Section 17.2.1 for native units per rotation.
+     */
+    m_motor.configAllowableClosedloopError(0, Constants.kPIDPivot, Constants.kTimeoutMs);
+
+    // * Config Position Closed Loop gains in slot0, tsypically kF stays zero.
+    m_motor.config_kF(Constants.kPIDPivot, Constants.kGains.kF, Constants.kTimeoutMs);
+    m_motor.config_kP(Constants.kPIDPivot, Constants.kGains.kP, Constants.kTimeoutMs);
+    m_motor.config_kI(Constants.kPIDPivot, Constants.kGains.kI, Constants.kTimeoutMs);
+    m_motor.config_kD(Constants.kPIDPivot, Constants.kGains.kD, Constants.kTimeoutMs);
+
+    /**
+     * Grab the 360 degree position of the MagEncoder's absolute position, and
+     * intitally set the relative sensor to match.
+     */
+    int absolutePosition = m_motor.getSensorCollection().getPulseWidthPosition();
+
+    /* Mask out overflows, keep bottom 12 bits */
+    absolutePosition &= 0xFFF;
+    if (Constants.kSensorPhase) {
+      absolutePosition *= -1;
+    }
+    if (Constants.kMotorInvert) {
+      absolutePosition *= -1;
+    }
+
+    /* Set the quadrature (relative) sensor to match absolute */
+    m_motor.setSelectedSensorPosition(absolutePosition, Constants.kPIDPivot, Constants.kTimeoutMs);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+   setDefaultCommand(new PivotWithJoystick());
+  }
+
+  /**
+   * The log method puts interesting information to the SmartDashboard.
+   */
+  public void log() {
+
+    SmartDashboard.putNumber("Pivot Target", m_motor.getClosedLoopTarget(Constants.kPIDPivot));
+    SmartDashboard.putNumber("Pivot Position", m_motor.getSelectedSensorPosition(Constants.kPIDPivot));
+  }
+
+  public void move(double power) {
+
+    m_motor.set(ControlMode.PercentOutput, (power / Constants.kPivotDamp));
+
+  }
+
+  public void moveto(double degrees) {
+
+    m_motor.set(ControlMode.Position, (degrees / 360) * Constants.kSensorUnitsPerRotation);
+
+  }
+
+  public boolean ontarget() {
+    if (m_motor.getClosedLoopError(Constants.kPIDPivot) < 1) {
+      return true;
+    }
+    return false;
+  }
+}
